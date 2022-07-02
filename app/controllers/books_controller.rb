0e80de9ab9fa02b6140869f77f7e59d9a14e590a
EC2 Default User <107891289+momoko-shj@users.noreply.github.com>
2022-07-02 20:36:41 +0000

    [start]Lesson3 Examination_Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,30 @@
 class BooksController < ApplicationController
   def new
-     @book=Book.new
+    
   end
 
   def index
-     @book=Book.new
+      @books=Book.all
+      @book=Book.new
   end
-  
+
   def create
-    book=Book.new(book_params)
+    book = Book.new(book_params)#新規でデータを取得する　createは内部処理のため@は不要
     book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id)#route.rbのパスで確認　idは自動生成される番号
   end
+  #
     
   def show
   end
 
   def edit
   end
-end
-
-
 
 private
 
     def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title, :body)#カラムに入れるデータを定義づけ
     end
+  
+end
