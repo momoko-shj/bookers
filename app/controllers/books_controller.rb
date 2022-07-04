b2c7f4dddfae861cd74dbf1ea782c9d76a0c179d
EC2 Default User <107891289+momoko-shj@users.noreply.github.com>
2022-07-04 01:48:16 +0000

    Run Click

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,8 @@ class BooksController < ApplicationController
   end
 
   def index
-      @books=Book.all
-      @book=Book.new
+      @books = Book.all
+      @book = Book.new
   end
 
   def create
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
   end
   
   def show
-    @book = Book.find(params[:id])
+   @book = Book.find(params[:id])
   end
 
   def edit
@@ -23,10 +23,17 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.fine(params[:id])
+    book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
   
 private
 
