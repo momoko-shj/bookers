5bc57a5d1cd0ab4d9f701c127161a14e2b00f092
EC2 Default User <107891289+momoko-shj@users.noreply.github.com>
2022-07-05 11:04:41 +0000

    [FINISH]Lesson2 Examination Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,9 +30,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      @book = Book.find(params[:id])
       render :edit
     end
     flash[:hoge]="Book was successfully updated."
